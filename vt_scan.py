683bb04ec8a0358ea091fcb91aa1613b2c3fc99e
Xavier Chapron <chapron.xavier@gmail.com>
2016-09-01 12:04:38 +0200

    working version to start a file, no more usable on linux

--- vt_scan.py
+++ vt_scan.py
@@ -6,10 +6,10 @@ from urllib import urlencode
 from urllib2 import Request, urlopen, HTTPError, URLError
 from time import sleep
 from re import search
-from os import system, remove
+from os import system
 from optparse import OptionParser
-from shutil import copyfile
 from os.path import dirname, join
+from tempfile import gettempdir
 
 parser = OptionParser("usage: %prog -f path_to_file [options]")
 parser.add_option("-f", "--file",
@@ -45,7 +45,7 @@ def search_on_vt(md5s, apikey):
     return loads(response.read())
 
 
-def run_vt_analyse(md5s_list, apikey):
+def run_vt_analyse(md5s_list, apikey, results):
     # Format the md5s_list for the request
     md5_request = ""
     for md5 in md5s_list:
@@ -59,29 +59,30 @@ def run_vt_analyse(md5s_list, apikey):
             answer_list = search_on_vt(md5_request, apikey)
         except ValueError:
             answer_list = None
-            output("### Error, VT refuse to answer, the script will retry in 30sec.")
+            print("### Error, VT refuse to answer, the script will retry in 30sec.")
             sleep(30)
         except HTTPError:
-            output("Your apikey %s seem to be refuse by VirusTotal." % apikey)
+            print("Your apikey %s seem to be refuse by VirusTotal." % apikey)
             exit_program()
         except URLError:
-            output("You should check your internet connexion")
+            print("You should check your internet connexion")
             exit_program()
 
     # Analyse the answer
     if len(md5s_list) == 1:
-        analyse_answer(answer_list, md5s_list)
+        analyse_answer(answer_list, md5s_list, results)
     else:
         for answer in answer_list:
-            analyse_answer(answer, md5s_list)
+            analyse_answer(answer, md5s_list, results)
 
 
-def analyse_answer(answer, md5s_list):
+def analyse_answer(answer, md5s_list, results):
     # Check if VT have found the associate the file
     if answer.get("response_code", 0) == 0:
         md5 = answer.get("resource", "error")
         filename = get_filename_for_md5(md5, md5s_list)
-        output("VirusTotal seems to not know file: %s with md5:%s." % (filename, md5))
+        print("VirusTotal seems to not know file: %s with md5:%s." % (filename, md5))
+        results["unknows"].append((filename, md5))
 
     else:
         # output answer
@@ -90,7 +91,12 @@ def analyse_answer(answer, md5s_list):
         positives = answer.get("positives", None)
         total = answer.get("total", None)
         url = "https://www.virustotal.com/latest-scan/" + md5
-        output("%s/%s for %s, more info at %s" % (positives, total, filename, url))
+        result = (positives, total, filename, url)
+        print("%s/%s for %s, more info at %s" % result)
+        if positives:
+            results["positives"].append(result)
+        else:
+            results["negatives"].append(result)
 
 
 def get_filename_for_md5(md5, md5s_list):
@@ -145,17 +151,14 @@ def exit_program():
 
 def run(options):
 
-    # Remove the old output file
-    system("echo %s > output.txt" % "VT_Scan:")
-
     if not options.path_to_file:
         try:
             with open("input.txt", 'r') as f:
                 if f.readline():
                     options.path_to_file = "input.txt"
         except IOError:
-            output('you must use an input file, save it as input.txt or use -f option in command line')
-            parser.error('you must use an input file, save it as input.txt or use -f option in command line')
+            print('you must use an input file, save it as input.txt or use -f option in command line')
+            exit_program()
 
 
     if not options.apikey:
@@ -163,59 +166,78 @@ def run(options):
             with open("apikey.txt", 'r') as f:
                 apikey = f.readline().replace("\n", "").replace(" ", "").replace("\r", "")
             if not apikey:
-                output('you must use an apikey, set it in apikey.txt or use -k option in command line')
-                parser.error('you must use an apikey, set it in apikey.txt or use -k option in command line')
+                print('you must use an apikey, set it in apikey.txt or use -k option in command line')
+                exit_program()
         except IOError:
-            output('you must use an apikey, set it in apikey.txt or use -k option in command line')
-            parser.error('you must use an apikey, set it in apikey.txt or use -k option in command line')
+            print('you must use an apikey, set it in apikey.txt or use -k option in command line')
+            exit_program()
     else:
         # We want to use by default the apikey from command line
         apikey = options.apikey
 
     # Tell the user which API key will be used
-    output("The script will use VT API key: '%s'" % apikey)
+    print("The script will use VT API key: '%s'" % apikey)
 
     # Remove issues with \n at the end of the filename
     options.path_to_file = options.path_to_file.replace("\n","")
-    output("The input file is %s" % options.path_to_file)
+    print("The input file is %s" % options.path_to_file)
 
     # Detect the logFile type
     with open(options.path_to_file, 'r') as f:
         file_type = get_file_type(f.readline())
-        output("The input file is detected as a %s log." % file_type)
+        print("The input file is detected as a %s log." % file_type)
 
     # Find the md5s in the file
     md5s_list = find_md5_in_file(options.path_to_file)
     if len(md5s_list) == 0:
-        output(
+        print(
           "Found 0 md5 in %s, if there is md5, convert the log file encoding to 'utf-8'."
           % options.path_to_file
         )
         exit_program()
-    output("Found %s different md5s in %s." % (len(md5s_list), options.path_to_file))
-    output("The analysis should take about %s min." % int(len(md5s_list) / 16 + 1))
+    print("Found %s different md5s in %s." % (len(md5s_list), options.path_to_file))
+    print("The analysis should take about %s min." % int(len(md5s_list) / 16 + 1))
+
+    results = {"unknows": [], "negatives": [], "positives": []}
 
     # Search on VT for each md5 by group of 4
     while len(md5s_list) >= 4:
-        run_vt_analyse(md5s_list[0:4], apikey)
+        run_vt_analyse(md5s_list[0:4], apikey, results)
         md5s_list = md5s_list[4:]
 
         # The VirusTotal public API allow 4 request each minute,
         # therefore we should wait 15sec between each request.
         sleep(15)
-    run_vt_analyse(md5s_list, apikey)
+    run_vt_analyse(md5s_list, apikey, results)
 
-    output("### End of analysis.")
 
-    save = raw_input("Do you want to save the log, if no the program will stop. [y/N] ")
-    if save == "y":
-        default_filename = "output.txt"
-        filename = raw_input("Enter a file name, the default is %s: " % default_filename)
-        if filename:
-            copyfile("output.txt", join(dirname(options.path_to_file), filename))
-            remove("output.txt")
-        exit_program()
-    remove("output.txt")
-    exit_program()
+    log_path = join(gettempdir(), "vt_scan.txt")
+    with open(log_path, 'w') as f:
+        f.write("VT_Scan by Chapi:\n")
+        f.write("The script will use VT API key: '%s'\n" % apikey)
+        f.write("The input file is %s\n" % options.path_to_file)
+        f.write("The input file is detected as a %s log.\n" % file_type)
+        f.write("Found %s different md5s in %s.\n" % (len(md5s_list), options.path_to_file))
+
+        if results["positives"]:
+            f.write("\n--- VirusTotal nonzero detections ---\n")
+            for result in results["positives"]:
+                f.write("%s/%s for %s, more info at %s\n" % result)
+
+        if results["unknows"]:
+            f.write("\n--- VirusTotal unknow files ---\n")
+            for result in results["unknows"]:
+                f.write("%s with md5:%s.\n" % result)
+
+        if results["negatives"]:
+            f.write("\n--- VirusTotal negative results ---\n")
+            for result in results["negatives"]:
+                f.write("%s/%s for %s, more info at %s\n" % result)
+
+        f.write("\n\n###End of analysis.")
+
+    print("### End of analysis.")
+
+    system("start %s" % log_path)
 
 run(options)
