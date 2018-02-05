640f45093722722d04941a3c1b781a33970212ed
Xavier Chapron <chapron.xavier@gmail.com>
2018-02-05 21:04:33 +0100

    vt_scan_gui.py: Add console and Use common config_file

--- vt_scan_gui.py
+++ vt_scan_gui.py
@@ -2,7 +2,7 @@
 
 import tkinter as tk
 from tkinter import filedialog
-import json
+from tkinter import scrolledtext
 import vt_scan
 from tempfile import gettempdir
 from os.path import join
@@ -11,27 +11,6 @@ from webbrowser import open as webopen
 
 # Full path to the output log
 log_path = join(gettempdir(), "vt_scan.html")
-
-config_file = "vt_scan_config.txt"
-config = {}
-
-
-def load_config():
-    config = {}
-    try:
-        with open(config_file, "r") as f:
-            config = json.load(f)
-    except json.JSONDecodeError:
-        print("JSON error")
-    except FileNotFoundError:
-        print("No config")
-        save_config()
-    return config
-
-
-def save_config():
-    with open(config_file, "w") as f:
-        json.dump(config, f)
 
 
 class simpleapp_tk(tk.Tk):
@@ -47,7 +26,6 @@ class simpleapp_tk(tk.Tk):
         self.results = {}
 
         self.apikey_string = tk.StringVar()
-        self.apikey_string.set(config.get("apikey", "no apikey"))
 
         apikey_label = tk.Label(self, anchor="w", bg="white", fg="black", text="Apikey:")
         apikey_label.grid(column=0, row=0, columnspan=3, sticky='EW')
@@ -70,14 +48,26 @@ class simpleapp_tk(tk.Tk):
         run_button = tk.Button(self, text="Run VT Scan", command=self.run_OnButtonClick)
         run_button.grid(column=5, row=2, columnspan=5)
 
+        self.console = scrolledtext.ScrolledText(undo=True)
+        self.console.grid(column=0, row=8, columnspan=18)
+        self.console.insert(tk.END, "Loading config...\n")
+        try:
+            self.config = vt_scan.load_config()
+            self.console.insert(tk.END, "Config found:\n%s\n" % str(self.config))
+        except vt_scan.ScriptError as e:
+            self.console.insert(tk.END, "\n/!\\ ERROR: %s\n" % e.message)
+
+        self.apikey_string.set(self.config.get("apikey", "no apikey"))
+
         self.grid_columnconfigure(0, weight=1)
         self.resizable(False, False)
         self.update()
 
     def apikey_save_OnButtonClick(self):
-        config["apikey"] = self.apikey_entry.get()
-        self.apikey_string.set(config.get("apikey", "no apikey"))
-        save_config()
+        self.config["apikey"] = self.apikey_entry.get()
+        self.apikey_string.set(self.config.get("apikey", "no apikey"))
+        vt_scan.save_config(self.config)
+        self.console.insert(tk.END, "Config: Saving apikey into config file\n")
 
     def file_dialog_OnButtonClick(self):
         input_file = filedialog.askopenfile(title='Input file')
@@ -86,26 +76,33 @@ class simpleapp_tk(tk.Tk):
             line_list = vt_scan.get_report_lines(self.input_file_string.get())
             self.file_type = vt_scan.get_file_type(line_list[0])
             self.md5s_list = vt_scan.find_md5_in_file(line_list, self.file_type)
+            self.console.insert(tk.END, "\nOpening: %s\n" % input_file)
+            self.console.insert(tk.END, "Found the file to be of type %s\n" % self.file_type)
+            self.console.insert(tk.END, "Found %s MD5s\n" % len(self.md5s_list))
 
     def run_OnButtonClick(self):
         try:
             if len(self.md5s_list) > 0:
-                self.results = vt_scan.run_vt_analyse(self.md5s_list, self.apikey_string.get())
+                try:
+                    apikey = self.config["apikey"]
+                except KeyError:
+                    raise vt_scan.ScriptError("You should configure an apikey")
+                self.results = vt_scan.run_vt_analyse(self.md5s_list, apikey)
 
                 # Create the output log
                 vt_scan.save_results(log_path, self.input_file_string.get(), self.file_type, len(self.md5s_list), self.results)
 
                 # Open the log
+                self.console.insert(tk.END, "\nScan complete, opening results\n")
                 webopen(log_path)
+            else:
+                raise vt_scan.ScriptError("You have to choose a file with MD5s")
 
         except vt_scan.ScriptError as e:
-            print("Catch an error:")
-            print(e.message)
+            self.console.insert(tk.END, "\n/!\\ ERROR: %s\n" % e.message)
 
 
 if __name__ == "__main__":
-    config = load_config()
-    print(config)
     app = simpleapp_tk(None)
     app.title('VT Scan GUI')
     app.mainloop()
