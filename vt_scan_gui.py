6fa33505d8360508643170e6d4974eb421b6390c
Xavier Chapron <chapron.xavier@gmail.com>
2018-02-11 11:05:41 +0100

    vt_scan_gui.py: Improve columns and rows handling

--- vt_scan_gui.py
+++ vt_scan_gui.py
@@ -44,36 +44,48 @@ class simpleapp_tk(tk.Tk):
         self.results = {}
         self.config_file = join(dirname(abspath(sys.argv[0])), config_file_name)
 
+        x_pos = 1
+        y_pos = 1
+
         # Apikey widgets
         apikey_label = tk.Label(self, anchor="w", fg="black", text="Apikey:")
-        apikey_label.place(x=c_to_x(1), y=r_to_y(1), width=c_to_x(6), height=r_to_y(2))
+        apikey_label.place(x=c_to_x(x_pos), y=r_to_y(y_pos), width=c_to_x(6), height=r_to_y(2))
+        x_pos += 6 + 1
 
         self.apikey_string = tk.StringVar()
         self.apikey_entry = tk.Entry(self, textvariable=self.apikey_string)
-        self.apikey_entry.place(x=c_to_x(1 + 6 + 1), y=r_to_y(1), width=c_to_x(40), height=r_to_y(2))
+        self.apikey_entry.place(x=c_to_x(x_pos), y=r_to_y(y_pos), width=c_to_x(40), height=r_to_y(2))
+        x_pos += 40 + 1
 
         apikey_save_button = tk.Button(self, text="Save", command=self.apikey_save_OnButtonClick)
-        apikey_save_button.place(x=c_to_x(1 + 6 + 1 + 40 + 1), y=r_to_y(1), width=c_to_x(9), height=r_to_y(2))
+        apikey_save_button.place(x=c_to_x(x_pos), y=r_to_y(y_pos), width=c_to_x(9), height=r_to_y(2))
+        x_pos = 1
+        y_pos += 2 + 1
 
         # Input file widgets
         apikey_label = tk.Label(self, anchor="w", fg="black", text="Input file:")
-        apikey_label.place(x=c_to_x(1), y=r_to_y(1 + 2 + 1), width=c_to_x(6), height=r_to_y(2))
+        apikey_label.place(x=c_to_x(x_pos), y=r_to_y(1 + 2 + 1), width=c_to_x(6), height=r_to_y(2))
+        x_pos += 6 + 1
 
         self.input_file_string = tk.StringVar()
         self.input_file_string.set("No input file selected")
         input_file_label = tk.Label(self, textvariable=self.input_file_string,
                                     anchor="w", bg="white", fg="black")
-        input_file_label.place(x=c_to_x(1 + 6 + 1), y=r_to_y(1 + 2 + 1), width=c_to_x(40), height=r_to_y(2))
+        input_file_label.place(x=c_to_x(x_pos), y=r_to_y(y_pos), width=c_to_x(40), height=r_to_y(2))
+        x_pos += 40 + 1
 
         self.file_dialog_button = tk.Button(self, text="Choose a file", command=self.file_dialog_OnButtonClick)
-        self.file_dialog_button.place(x=c_to_x(1 + 6 + 1 + 40 + 1), y=r_to_y(1 + 2 + 1), width=c_to_x(9), height=r_to_y(2))
+        self.file_dialog_button.place(x=c_to_x(x_pos), y=r_to_y(y_pos), width=c_to_x(9), height=r_to_y(2))
+        x_pos = 1
+        y_pos += 2 + 1
 
         # Run widget
         run_button = tk.Button(self, text="Run VT Scan", command=self.run_OnButtonClick)
-        run_button.place(x=(app_w - c_to_x(15)) / 2, y=r_to_y(1 + 2 + 1 + 2 + 1), width=c_to_x(15), height=r_to_y(2))
+        run_button.place(x=(app_w - c_to_x(15)) / 2, y=r_to_y(y_pos), width=c_to_x(15), height=r_to_y(2))
+        y_pos += 2 + 1
 
         self.console = scrolledtext.ScrolledText(undo=True)
-        self.console.place(x=0, y=r_to_y(1 + 2 + 1 + 2 + 1 + 2 + 2), width=app_w, height=app_h - r_to_y(1 + 2 + 1 + 2 + 1 + 2 + 2))
+        self.console.place(x=0, y=r_to_y(y_pos), width=app_w, height=app_h - r_to_y(y_pos))
         self.console.insert(tk.END, "Loading config...\n")
         try:
             self.config = vt_scan.load_config(self.config_file)
